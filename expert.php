15fbcbfd0469dc77595c668768fe6bdb7bacdac2
Yasser <yasserbal@icloud.com>
2022-06-20 10:39:29 +0200

    Exercises completed

--- expert.php
+++ expert.php
@@ -45,30 +45,88 @@ function combineNames($str1 = "", $str2 = "")
     $params = [$str1, $str2];
     foreach ($params as &$param) {
         if ($param === "") {
-            $param = randomHeroName();
+            if ($params[0] === "") {
+                $order = 0;
+            } else {
+                $order = 1;
+            }
+            $param = randomHeroName($order);
         }
     }
     return implode(" - ", $params);
 }
-
-
-// function randomGenerate($arr, $amount) {
-//     for ($i = $amount; $i > 0; $i--) {
-//         array_push($arr, randomHeroName());
-//     }
-
-//     return $amount;
-// }
-
-function randomHeroName()
+function randomHeroName($firstOrLast)
+//If First of last = 0 will take from first names
+//If First or last = 1 will take from last names
 {
     $hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black", "Carol"];
     $hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil", "marvel"];
     $heroes = [$hero_firstnames, $hero_lastnames];
-    $randname = $heroes[rand(0, count($heroes))][rand(0, 10)];
-    // $heroes[rand(0,count($heroes))][rand(0, 10)];
-
+    $randname = $heroes[$firstOrLast][rand(0, 10)];
     return $randname;
 }
-
 echo "Here is the name: " . combineNames();
+
+new_exercise(7);
+function copyright(int $year)
+{
+    return "&copy; $year BeCode";
+}
+//print the copyright
+echo copyright((int)date("Y"));
+
+new_exercise(8);
+function login(string $email, string $password)
+{
+    echo "<br>";
+    if ($email === 'john@example.be' && $password === 'pocahontas') {
+        return 'Welcome John Smith';
+    } else {
+        return 'No access';
+    }
+}
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas');
+//no access
+echo login('john@example.be', 'dfgidfgdfg');
+//no access
+echo login('wrong@example.be', 'wrong');
+//you can change things again!
+
+new_exercise(9);
+function isLinkValid(string $link)
+{
+    $unacceptables = array('https:', '.doc', '.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+
+    foreach ($unacceptables as $unacceptable) {
+        if (strpos($link, $unacceptable) !== false) {
+            return 'Unacceptable Found<br />';
+        }
+    }
+    return 'Acceptable<br />';
+}
+//invalid link
+echo isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+echo isLinkValid('https://google.com');
+//VALID link
+echo isLinkValid('http://google.com');
+//VALID link
+echo isLinkValid('http://google.com/test.txt');
+
+
+new_exercise(10);
+
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+$checkListLength = count($areTheseFruits);
+for ($i = 0; $i < $checkListLength; $i++) {
+    if (!in_array($areTheseFruits[$i], $validFruits, true)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits);//do not change this
